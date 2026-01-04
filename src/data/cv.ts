2f520ada6a3ccf5da21bb194d917553ba5ff31ef
DavidJGChen <33329617+DavidJGChen@users.noreply.github.com>
2026-01-04 02:53:25 -0800

    revamp website using astro

---
+++ src/data/cv.ts
@@ -0,0 +1,80 @@
+export const experiences = [
+	{
+		company: 'Meta Platforms',
+		time: '2021 - 2025',
+		title: 'Software Engineer',
+		location: 'Menlo Park, CA',
+		description: 'Product software engineer on Facebook.',
+	},
+	{
+		company: 'Facebook',
+		time: '2020',
+		title: 'Software Engineer Intern',
+		location: 'Menlo Park, CA',
+		description: 'AR/VR SWE intern on Horizon Worlds.',
+	},
+	{
+		company: 'GoDaddy',
+		time: '2019',
+		title: 'Software Development Engineer Intern',
+		location: 'Kirkland, WA',
+		description: '',
+	},
+	{
+		company: 'Akamai Technologies',
+		time: '2018',
+		title: 'Software Development Engineer in Test Intern',
+		location: 'Cambridge, MA',
+		description: '',
+	},
+];
+
+export const education = [
+	{
+		school: 'Carnegie Mellon University: School of Computer Science',
+		time: '2017 - 2021',
+		degree: 'Bachelor’s in Computer Science',
+		location: 'Pittsburgh, PA',
+		description: 'Simultaneously pursued a minor in mathematics.',
+	},
+	{
+		school: 'Stanford University',
+		time: '2022 - 2025',
+		degree: 'Artificial Intelligence Graduate Certificate',
+		location: 'Stanford, CA',
+		description: 'Completed various ML and stats coursework + projects',
+	},
+	{
+		school: 'Stanford University',
+		time: '2025 - 2027',
+		degree: 'Master’s in Computer Science: AI',
+		location: 'Stanford, CA',
+		description: 'Studying more ML and statistics!',
+	},
+];
+
+export const skills = [
+	{
+		title: 'Section under construction!',
+		description: 'Check back in a few weeks!',
+	},
+];
+
+export const publications = [
+	{
+		title: 'Exploring Bandit Algorithms',
+		authors: 'David Chen',
+		journal: 'CS 221',
+		time: '2025',
+		link: 'bandits.pdf',
+		abstract: 'This project evaluates classic and modern stochastic multi-armed bandit algorithms by comparing their theoretical regret bounds against empirical performance and runtime. I developed a simulation framework to test strategies such as ϵ-greedy, Thompson sampling, and information-directed sampling across independent and linear bandit settings. This comprehensive analysis aims to provide both quantitative benchmarks and qualitative insights into how different exploration-exploitation strategies behave in practice.',
+	},
+	{
+		title: 'Evaluating Stitching Capabilities of RvS Transformer Algorithms',
+		authors: 'David Chen',
+		journal: 'CS 234',
+		time: '2024',
+		link: 'stitching-paper.pdf',
+		abstract: 'This paper benchmarks the ability of Transformer-based reinforcement learning methods to "stitch" suboptimal trajectories into optimal policies across challenging AntMaze environments. We introduce an enhanced Waypoint Transformer with a refined waypoint selection strategy that improves performance. These contributions provide a comprehensive evaluation of current sequence modeling approaches and suggest new avenues for goal-conditioned behavior cloning.',
+	},
+];
---
+++ src/settings.ts
@@ -0,0 +1,41 @@
+export const profile = {
+	fullName: 'David Chen',
+	title: 'Master\'s student',
+	institute: 'Stanford University',
+	author_name: '', // Author name to be highlighted in the papers section
+	research_areas: [
+		{ title: 'Reinforcement Learning', description: 'Multi-armed bandits, offline RL, etc.', field: 'computer-science' },
+		{ title: 'Mathematical Statistics', description: 'Currently taking a subset of the Stats PhD sequence.', field: 'mathematics' },
+	],
+}
+
+// Set equal to an empty string to hide the icon that you don't want to display
+export const social = {
+	email: 'dchen11@stanford.edu',
+	linkedin: 'https://www.linkedin.com/in/davidjgchen',
+	x: '',
+	bluesky: '',
+	github: 'https://github.com/DavidJGChen',
+	gitlab: '',
+	scholar: '',
+	inspire: '',
+	arxiv: '',
+	orcid: '',
+}
+
+export const template = {
+	website_url: 'https://localhost:4321', // Astro needs to know your site’s deployed URL to generate a sitemap. It must start with http:// or https://
+	menu_left: false,
+	transitions: true,
+	lightTheme: 'light', // Select one of the Daisy UI Themes or create your own
+	darkTheme: 'dark', // Select one of the Daisy UI Themes or create your own
+	excerptLength: 200,
+	postPerPage: 5,
+    base: '' // Repository name starting with /
+}
+
+export const seo = {
+	default_title: 'David Chen | MS in Computer Science: Artificial Intelligence at Stanford University',
+	default_description: 'David Chen is a Master\'s student in Computer Science at Stanford University, studying artificial intelligence and statistics. He is especially interested in machine learning theory, and is currently learning more about statistics, decision-making under uncertainty, and combinatorial optimization.',
+	default_image: '/images/astro-academia.png',
+}
